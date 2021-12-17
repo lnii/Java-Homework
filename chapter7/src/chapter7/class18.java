77b5672cf3df63531bc1a2f2ffc443b61ebefd69
lnii <92147855+lnii@users.noreply.github.com>
2021-12-17 20:16:12 +0800

    Add files via upload

---
+++ chapter7/src/chapter7/class18.java
@@ -0,0 +1,10 @@
+package chapter7;
+
+public class class18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ chapter7/src/chapter7/class34.java
@@ -0,0 +1,10 @@
+package chapter7;
+
+public class class34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
